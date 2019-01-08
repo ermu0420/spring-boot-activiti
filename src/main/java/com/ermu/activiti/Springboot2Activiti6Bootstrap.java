a92fe65cf4d0aa73610ccce4f30a1cb94fc78db6
ermu0420 <992124332@qq.com>
2019-01-08 16:24:13 +0800

    initbranch

---
+++ src/main/java/com/ermu/activiti/Springboot2Activiti6Bootstrap.java
@@ -0,0 +1,12 @@
+package com.ermu.activiti;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Springboot2Activiti6Bootstrap {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Springboot2Activiti6Bootstrap.class).run(args);
+    }
+
+}
